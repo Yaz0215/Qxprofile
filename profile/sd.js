84ee2059e501619328beaf4ca907ba0e7677154e
Yaz0215 <68006284+Yaz0215@users.noreply.github.com>
2023-01-20 17:34:01 +0800

    Update sd.js

--- profile/sd.js
+++ profile/sd.js
@@ -2,6 +2,7 @@
 圈x
 保存这个js文件、到本地Script下面
 再去复制一下内容到对应的配置
+*/
 
 [rewrite_local]
 #本地脚本
@@ -11,7 +12,7 @@ https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body ng-pro
 
 MITM = buy.itunes.apple.com
 
-*/
+
 
 var obj = JSON.parse($response.body); 
  
